6967869f0c81b3b7fc6dde760a69b33e010916ff
PabloChavez03 <pablooscarchavez@gmail.com>
2023-11-22 18:41:43 -0300

    add root app

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, SafeAreaView, StyleSheet, View } from 'react-native';
+import Home from './src/views/home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style='light' />
+      <View style={{ flex: 1 }}>
+        <Home />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'rgba(7, 26, 93, 255)',
+    paddingTop: Platform.OS === 'android' ? 35 : 0,
   },
 });
